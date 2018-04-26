0ad8deeb4dee1fac3148fbb5d9483224245d54bd
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-04-26 14:06:20 -0700

    run shell in run.cmd
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- run.cmd
+++ run.cmd
@@ -2,4 +2,4 @@ docker stop fabric.baseos
 docker rm fabric.baseos
 docker build -t healthcatalyst/fabric.baseos . 
 
-docker run --rm --name fabric.baseos -t healthcatalyst/fabric.baseos
+docker run --rm --name fabric.baseos -it healthcatalyst/fabric.baseos sh
